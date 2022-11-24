d2d42c52a2e61066091dfc4606fd9a65fc4725a4
sarah <>
2022-11-24 22:55:53 +0100

    more efficient packing

--- src/pack_operands.rs
+++ src/pack_operands.rs
@@ -1,6 +1,153 @@
 use crate::simd::Simd;
 
 #[inline(always)]
+fn quick_zero<T: Copy>(slice: &mut [T]) {
+    let n = slice.len();
+    match n {
+        1 => unsafe { *(slice.as_mut_ptr() as *mut [T; 1]) = core::mem::zeroed() },
+        2 => unsafe { *(slice.as_mut_ptr() as *mut [T; 2]) = core::mem::zeroed() },
+        3 => unsafe { *(slice.as_mut_ptr() as *mut [T; 3]) = core::mem::zeroed() },
+        4 => unsafe { *(slice.as_mut_ptr() as *mut [T; 4]) = core::mem::zeroed() },
+        5 => unsafe { *(slice.as_mut_ptr() as *mut [T; 5]) = core::mem::zeroed() },
+        6 => unsafe { *(slice.as_mut_ptr() as *mut [T; 6]) = core::mem::zeroed() },
+        7 => unsafe { *(slice.as_mut_ptr() as *mut [T; 7]) = core::mem::zeroed() },
+        8 => unsafe { *(slice.as_mut_ptr() as *mut [T; 8]) = core::mem::zeroed() },
+        9 => unsafe { *(slice.as_mut_ptr() as *mut [T; 9]) = core::mem::zeroed() },
+        10 => unsafe { *(slice.as_mut_ptr() as *mut [T; 10]) = core::mem::zeroed() },
+        11 => unsafe { *(slice.as_mut_ptr() as *mut [T; 11]) = core::mem::zeroed() },
+        12 => unsafe { *(slice.as_mut_ptr() as *mut [T; 12]) = core::mem::zeroed() },
+        13 => unsafe { *(slice.as_mut_ptr() as *mut [T; 13]) = core::mem::zeroed() },
+        14 => unsafe { *(slice.as_mut_ptr() as *mut [T; 14]) = core::mem::zeroed() },
+        15 => unsafe { *(slice.as_mut_ptr() as *mut [T; 15]) = core::mem::zeroed() },
+        16 => unsafe { *(slice.as_mut_ptr() as *mut [T; 16]) = core::mem::zeroed() },
+        17 => unsafe { *(slice.as_mut_ptr() as *mut [T; 17]) = core::mem::zeroed() },
+        18 => unsafe { *(slice.as_mut_ptr() as *mut [T; 18]) = core::mem::zeroed() },
+        19 => unsafe { *(slice.as_mut_ptr() as *mut [T; 19]) = core::mem::zeroed() },
+        20 => unsafe { *(slice.as_mut_ptr() as *mut [T; 20]) = core::mem::zeroed() },
+        21 => unsafe { *(slice.as_mut_ptr() as *mut [T; 21]) = core::mem::zeroed() },
+        22 => unsafe { *(slice.as_mut_ptr() as *mut [T; 22]) = core::mem::zeroed() },
+        23 => unsafe { *(slice.as_mut_ptr() as *mut [T; 23]) = core::mem::zeroed() },
+        24 => unsafe { *(slice.as_mut_ptr() as *mut [T; 24]) = core::mem::zeroed() },
+        25 => unsafe { *(slice.as_mut_ptr() as *mut [T; 25]) = core::mem::zeroed() },
+        26 => unsafe { *(slice.as_mut_ptr() as *mut [T; 26]) = core::mem::zeroed() },
+        27 => unsafe { *(slice.as_mut_ptr() as *mut [T; 27]) = core::mem::zeroed() },
+        28 => unsafe { *(slice.as_mut_ptr() as *mut [T; 28]) = core::mem::zeroed() },
+        29 => unsafe { *(slice.as_mut_ptr() as *mut [T; 29]) = core::mem::zeroed() },
+        30 => unsafe { *(slice.as_mut_ptr() as *mut [T; 30]) = core::mem::zeroed() },
+        31 => unsafe { *(slice.as_mut_ptr() as *mut [T; 31]) = core::mem::zeroed() },
+        32 => unsafe { *(slice.as_mut_ptr() as *mut [T; 32]) = core::mem::zeroed() },
+        33 => unsafe { *(slice.as_mut_ptr() as *mut [T; 33]) = core::mem::zeroed() },
+        34 => unsafe { *(slice.as_mut_ptr() as *mut [T; 34]) = core::mem::zeroed() },
+        35 => unsafe { *(slice.as_mut_ptr() as *mut [T; 35]) = core::mem::zeroed() },
+        36 => unsafe { *(slice.as_mut_ptr() as *mut [T; 36]) = core::mem::zeroed() },
+        37 => unsafe { *(slice.as_mut_ptr() as *mut [T; 37]) = core::mem::zeroed() },
+        38 => unsafe { *(slice.as_mut_ptr() as *mut [T; 38]) = core::mem::zeroed() },
+        39 => unsafe { *(slice.as_mut_ptr() as *mut [T; 39]) = core::mem::zeroed() },
+        40 => unsafe { *(slice.as_mut_ptr() as *mut [T; 40]) = core::mem::zeroed() },
+        41 => unsafe { *(slice.as_mut_ptr() as *mut [T; 41]) = core::mem::zeroed() },
+        42 => unsafe { *(slice.as_mut_ptr() as *mut [T; 42]) = core::mem::zeroed() },
+        43 => unsafe { *(slice.as_mut_ptr() as *mut [T; 43]) = core::mem::zeroed() },
+        44 => unsafe { *(slice.as_mut_ptr() as *mut [T; 44]) = core::mem::zeroed() },
+        45 => unsafe { *(slice.as_mut_ptr() as *mut [T; 45]) = core::mem::zeroed() },
+        46 => unsafe { *(slice.as_mut_ptr() as *mut [T; 46]) = core::mem::zeroed() },
+        47 => unsafe { *(slice.as_mut_ptr() as *mut [T; 47]) = core::mem::zeroed() },
+        48 => unsafe { *(slice.as_mut_ptr() as *mut [T; 48]) = core::mem::zeroed() },
+        49 => unsafe { *(slice.as_mut_ptr() as *mut [T; 49]) = core::mem::zeroed() },
+        50 => unsafe { *(slice.as_mut_ptr() as *mut [T; 50]) = core::mem::zeroed() },
+        51 => unsafe { *(slice.as_mut_ptr() as *mut [T; 51]) = core::mem::zeroed() },
+        52 => unsafe { *(slice.as_mut_ptr() as *mut [T; 52]) = core::mem::zeroed() },
+        53 => unsafe { *(slice.as_mut_ptr() as *mut [T; 53]) = core::mem::zeroed() },
+        54 => unsafe { *(slice.as_mut_ptr() as *mut [T; 54]) = core::mem::zeroed() },
+        55 => unsafe { *(slice.as_mut_ptr() as *mut [T; 55]) = core::mem::zeroed() },
+        56 => unsafe { *(slice.as_mut_ptr() as *mut [T; 56]) = core::mem::zeroed() },
+        57 => unsafe { *(slice.as_mut_ptr() as *mut [T; 57]) = core::mem::zeroed() },
+        58 => unsafe { *(slice.as_mut_ptr() as *mut [T; 58]) = core::mem::zeroed() },
+        59 => unsafe { *(slice.as_mut_ptr() as *mut [T; 59]) = core::mem::zeroed() },
+        60 => unsafe { *(slice.as_mut_ptr() as *mut [T; 60]) = core::mem::zeroed() },
+        61 => unsafe { *(slice.as_mut_ptr() as *mut [T; 61]) = core::mem::zeroed() },
+        62 => unsafe { *(slice.as_mut_ptr() as *mut [T; 62]) = core::mem::zeroed() },
+        63 => unsafe { *(slice.as_mut_ptr() as *mut [T; 63]) = core::mem::zeroed() },
+        64 => unsafe { *(slice.as_mut_ptr() as *mut [T; 64]) = core::mem::zeroed() },
+        _ => {
+            for value in slice {
+                *value = unsafe { core::mem::zeroed() };
+            }
+        }
+    }
+}
+
+#[inline(always)]
+unsafe fn quick_copy<T: Copy>(dst: *mut T, src: *const T, n: usize) {
+    match n {
+        1 => unsafe { *(dst as *mut [T; 1]) = *(src as *const [T; 1]) },
+        2 => unsafe { *(dst as *mut [T; 2]) = *(src as *const [T; 2]) },
+        3 => unsafe { *(dst as *mut [T; 3]) = *(src as *const [T; 3]) },
+        4 => unsafe { *(dst as *mut [T; 4]) = *(src as *const [T; 4]) },
+        5 => unsafe { *(dst as *mut [T; 5]) = *(src as *const [T; 5]) },
+        6 => unsafe { *(dst as *mut [T; 6]) = *(src as *const [T; 6]) },
+        7 => unsafe { *(dst as *mut [T; 7]) = *(src as *const [T; 7]) },
+        8 => unsafe { *(dst as *mut [T; 8]) = *(src as *const [T; 8]) },
+        9 => unsafe { *(dst as *mut [T; 9]) = *(src as *const [T; 9]) },
+        10 => unsafe { *(dst as *mut [T; 10]) = *(src as *const [T; 10]) },
+        11 => unsafe { *(dst as *mut [T; 11]) = *(src as *const [T; 11]) },
+        12 => unsafe { *(dst as *mut [T; 12]) = *(src as *const [T; 12]) },
+        13 => unsafe { *(dst as *mut [T; 13]) = *(src as *const [T; 13]) },
+        14 => unsafe { *(dst as *mut [T; 14]) = *(src as *const [T; 14]) },
+        15 => unsafe { *(dst as *mut [T; 15]) = *(src as *const [T; 15]) },
+        16 => unsafe { *(dst as *mut [T; 16]) = *(src as *const [T; 16]) },
+        17 => unsafe { *(dst as *mut [T; 17]) = *(src as *const [T; 17]) },
+        18 => unsafe { *(dst as *mut [T; 18]) = *(src as *const [T; 18]) },
+        19 => unsafe { *(dst as *mut [T; 19]) = *(src as *const [T; 19]) },
+        20 => unsafe { *(dst as *mut [T; 20]) = *(src as *const [T; 20]) },
+        21 => unsafe { *(dst as *mut [T; 21]) = *(src as *const [T; 21]) },
+        22 => unsafe { *(dst as *mut [T; 22]) = *(src as *const [T; 22]) },
+        23 => unsafe { *(dst as *mut [T; 23]) = *(src as *const [T; 23]) },
+        24 => unsafe { *(dst as *mut [T; 24]) = *(src as *const [T; 24]) },
+        25 => unsafe { *(dst as *mut [T; 25]) = *(src as *const [T; 25]) },
+        26 => unsafe { *(dst as *mut [T; 26]) = *(src as *const [T; 26]) },
+        27 => unsafe { *(dst as *mut [T; 27]) = *(src as *const [T; 27]) },
+        28 => unsafe { *(dst as *mut [T; 28]) = *(src as *const [T; 28]) },
+        29 => unsafe { *(dst as *mut [T; 29]) = *(src as *const [T; 29]) },
+        30 => unsafe { *(dst as *mut [T; 30]) = *(src as *const [T; 30]) },
+        31 => unsafe { *(dst as *mut [T; 31]) = *(src as *const [T; 31]) },
+        32 => unsafe { *(dst as *mut [T; 32]) = *(src as *const [T; 32]) },
+        33 => unsafe { *(dst as *mut [T; 33]) = *(src as *const [T; 33]) },
+        34 => unsafe { *(dst as *mut [T; 34]) = *(src as *const [T; 34]) },
+        35 => unsafe { *(dst as *mut [T; 35]) = *(src as *const [T; 35]) },
+        36 => unsafe { *(dst as *mut [T; 36]) = *(src as *const [T; 36]) },
+        37 => unsafe { *(dst as *mut [T; 37]) = *(src as *const [T; 37]) },
+        38 => unsafe { *(dst as *mut [T; 38]) = *(src as *const [T; 38]) },
+        39 => unsafe { *(dst as *mut [T; 39]) = *(src as *const [T; 39]) },
+        40 => unsafe { *(dst as *mut [T; 40]) = *(src as *const [T; 40]) },
+        41 => unsafe { *(dst as *mut [T; 41]) = *(src as *const [T; 41]) },
+        42 => unsafe { *(dst as *mut [T; 42]) = *(src as *const [T; 42]) },
+        43 => unsafe { *(dst as *mut [T; 43]) = *(src as *const [T; 43]) },
+        44 => unsafe { *(dst as *mut [T; 44]) = *(src as *const [T; 44]) },
+        45 => unsafe { *(dst as *mut [T; 45]) = *(src as *const [T; 45]) },
+        46 => unsafe { *(dst as *mut [T; 46]) = *(src as *const [T; 46]) },
+        47 => unsafe { *(dst as *mut [T; 47]) = *(src as *const [T; 47]) },
+        48 => unsafe { *(dst as *mut [T; 48]) = *(src as *const [T; 48]) },
+        49 => unsafe { *(dst as *mut [T; 49]) = *(src as *const [T; 49]) },
+        50 => unsafe { *(dst as *mut [T; 50]) = *(src as *const [T; 50]) },
+        51 => unsafe { *(dst as *mut [T; 51]) = *(src as *const [T; 51]) },
+        52 => unsafe { *(dst as *mut [T; 52]) = *(src as *const [T; 52]) },
+        53 => unsafe { *(dst as *mut [T; 53]) = *(src as *const [T; 53]) },
+        54 => unsafe { *(dst as *mut [T; 54]) = *(src as *const [T; 54]) },
+        55 => unsafe { *(dst as *mut [T; 55]) = *(src as *const [T; 55]) },
+        56 => unsafe { *(dst as *mut [T; 56]) = *(src as *const [T; 56]) },
+        57 => unsafe { *(dst as *mut [T; 57]) = *(src as *const [T; 57]) },
+        58 => unsafe { *(dst as *mut [T; 58]) = *(src as *const [T; 58]) },
+        59 => unsafe { *(dst as *mut [T; 59]) = *(src as *const [T; 59]) },
+        60 => unsafe { *(dst as *mut [T; 60]) = *(src as *const [T; 60]) },
+        61 => unsafe { *(dst as *mut [T; 61]) = *(src as *const [T; 61]) },
+        62 => unsafe { *(dst as *mut [T; 62]) = *(src as *const [T; 62]) },
+        63 => unsafe { *(dst as *mut [T; 63]) = *(src as *const [T; 63]) },
+        64 => unsafe { *(dst as *mut [T; 64]) = *(src as *const [T; 64]) },
+        _ => core::ptr::copy_nonoverlapping(src, dst, n),
+    }
+}
+
+#[inline(always)]
 unsafe fn pack_generic_inner_loop<T: Copy, const N: usize, const DST_WIDTH: usize>(
     mut dst: *mut T,
     mut src: *const T,
@@ -66,15 +213,28 @@ unsafe fn pack_generic_inner_loop<T: Copy, const N: usize, const DST_WIDTH: usiz
             }
         }
     } else {
-        for _ in 0..k {
-            for j in 0..src_width {
-                *dst.add(j) = *src.offset(j as isize * src_rs);
+        if src_rs == 1 {
+            for _ in 0..k {
+                quick_copy(dst, src, src_width);
+                quick_zero(core::slice::from_raw_parts_mut(
+                    dst.add(src_width),
+                    DST_WIDTH - src_width,
+                ));
+                src = src.wrapping_offset(src_cs);
+                dst = dst.add(DST_WIDTH);
             }
-            for j in src_width..DST_WIDTH {
-                *dst.add(j) = core::mem::zeroed::<T>();
+        } else {
+            for _ in 0..k {
+                for j in 0..src_width {
+                    *dst.add(j) = *src.offset(j as isize * src_rs);
+                }
+                quick_zero(core::slice::from_raw_parts_mut(
+                    dst.add(src_width),
+                    DST_WIDTH - src_width,
+                ));
+                src = src.wrapping_offset(src_cs);
+                dst = dst.add(DST_WIDTH);
             }
-            src = src.wrapping_offset(src_cs);
-            dst = dst.add(DST_WIDTH);
         }
     }
 }
